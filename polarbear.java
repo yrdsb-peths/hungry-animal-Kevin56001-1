b4833a0fba019b7838360d52ff9217e3d636a53a
Kevin <349292235@gapps.yrdsb.ca>
2022-05-03 10:32:22 -0400

    added polarbear

---
+++ polarbear.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class polarbear here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class polarbear extends Actor
+{
+    /**
+     * Act - do whatever the polarbear wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+         if(Greenfoot.isKeyDown("a")){
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            move(2);
+        }
+        
+    }
+}
